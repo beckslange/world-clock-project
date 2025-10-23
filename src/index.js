213d65a0cf63eb2e3e4a80a70335ba65c0894ae3
beckslange <beckslange96@gmail.com>
2025-10-22 21:16:24 -0600

    added ids to html, and added basic js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
 
+  losAngelesDateElement.innerHTML = losAngelesTime.format("dddd, MMM Do, YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let SydneyElement = document.querySelector("#sydney");
+  let SydneyDateElement = SydneyElement.querySelector(".date");
+  let SydneyTimeElement = SydneyElement.querySelector(".time");
+  let SydneyTime = moment().tz("Australia/Sydney");
+
+  SydneyDateElement.innerHTML = SydneyTime.format("dddd, MMM Do, YYYY");
+  SydneyTimeElement.innerHTML = SydneyTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
